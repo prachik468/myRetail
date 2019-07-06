e95295ea348f3e541f4d5716851cb087667916a7
prachi <prachi@10.0.0.240>
2019-07-05 22:29:58 -0500

    formatting

--- src/main/java/com/myretail/application/Application.java
+++ src/main/java/com/myretail/application/Application.java
@@ -13,12 +13,11 @@ import com.myretail.repository.ProductRepository;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-
 @SpringBootApplication
 @ComponentScan(basePackages = "com.*")
 @EnableMongoRepositories(basePackageClasses = ProductRepository.class)
 @EnableSwagger2
-public class Application  implements CommandLineRunner {
+public class Application implements CommandLineRunner {
 
 	@Autowired
 	private ProductRepository productRepository;
@@ -30,8 +29,8 @@ public class Application  implements CommandLineRunner {
 	// pre-populate some price data in data store(mongodb)
 	@Override
 	public void run(String... args) throws Exception {
-		Price price = new Price("13860428",13860428,14.49,"USD");
-		Product product = new Product(13860428,"The Big Lebowski (Blu-ray) (Widescreen)",price);
+		Price price = new Price("13860428", 13860428, 14.49, "USD");
+		Product product = new Product(13860428, "The Big Lebowski (Blu-ray) (Widescreen)", price);
 		productRepository.save(product);
 	}
 }
