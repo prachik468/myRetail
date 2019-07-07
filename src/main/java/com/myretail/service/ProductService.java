bcad5ff56ec5145e72ba3ee03a5afa13d2038876
prachik468 <40764968+prachik468@users.noreply.github.com>
2019-07-07 02:23:05 -0500

    clean up

--- src/main/java/com/myretail/service/ProductService.java
+++ src/main/java/com/myretail/service/ProductService.java
@@ -5,8 +5,8 @@ import com.myretail.model.Product;
 
 public interface ProductService {
 	
-	public Product getProduct(long id) throws ProductNotFoundException ;
+	Product getProduct(long id) throws ProductNotFoundException ;
 	
-	public Product updatePrice(long id, Product product) throws ProductNotFoundException ;
+	Product updatePrice(long id, Product product) throws ProductNotFoundException ;
 
 }
