64b63e5fb6f650d10ddcbe89f9dd44ca3d332c9a
prachik468 <40764968+prachik468@users.noreply.github.com>
2019-07-12 00:14:31 -0500

    Update ProductRepository.java

--- src/main/java/com/myretail/repository/ProductRepository.java
+++ src/main/java/com/myretail/repository/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.myretail.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.myretail.model.Product;
 
-public interface ProductRepository extends MongoRepository<Product, Double> {
+@Repository
+public interface ProductRepository extends MongoRepository<Product, Long> {
 	Product findByProductId(long productId);
 }
